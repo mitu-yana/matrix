12c5faabe0cb1ca04bee22e01f06918d676e6375
mitu-yana <mitu-yana@mail.ru>
2021-12-19 01:38:01 +0300

    popitka1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,17 +9,17 @@ public class Main {
         System.out.println("введите количество столбцов");
         int stlb = L.nextInt();
         double[][] twoDimArray = new double[str][stlb];
-        for (int i = 0; i < str; i++){  // идём по строкам
-            for (int j = 0; j < stlb;  j++){  // идём по столбцам
+        for (int i = 0; i < str; i++){
+            for (int j = 0; j < stlb;  j++){
                 System.out.println("введите элемент матрицы");
                 twoDimArray[i][j]=L.nextDouble();
             }
             }
-            for (int i = 0; i < str; i++){  // идём по строкам
-                for (int j = 0; j < stlb;  j++){  // идём по столбцам
-                System.out.printf(" %.2f ",twoDimArray[ i][j]*3);  // вывод элемента
+            for (int i = 0; i < str; i++){  //
+                for (int j = 0; j < stlb;  j++){
+                System.out.printf(" %.2f ",twoDimArray[ i][j]*3);
             }
-            System.out.println(); // перенос строки ради визуального сохранения табличной формы
+            System.out.println();
         }
     }
 }
